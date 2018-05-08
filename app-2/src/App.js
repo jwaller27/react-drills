4bb40587ab902259221ae25d625cd77c0ac786c7
Jonathan Waller <waller.j27@gmail.com>
2018-05-07 23:38:14 -0500

    Set 1, Quesiton 2 final

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(){
+    super();
+    this.state={
+      friends: ["Billy",'Bobby','Teddy','Elroy']
+    }
+  };
+    
+  render(){
+    let listOfFriends = this.state.friends.map((value,index)=>{
+    return(
+      <h2>{value}</h2>
+    )
+  });
+
+    return(
+      <div className = "App">
+      {listOfFriends}
       </div>
-    );
+    )
   }
-}
+};
 
 export default App;
