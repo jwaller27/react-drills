1a0e6750b031ded1c7f75ffd76f280b07cd7e158
Jonathan Waller <waller.j27@gmail.com>
2018-05-07 21:47:49 -0500

    q1.2 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -6,23 +6,24 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      message:''
+      friends: ["Billy",'Bobby','Teddy','Elroy']
     }
-  }
+  };
+    
+  render(){
+    let listOfFriends = this.state.friends.map((value,index)=>{
+    return(
+      <h2>{value}</h2>
+    )
+  });
 
-  handleChange(val){
-    this.setState({message:val})
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <input onChange={(e)=>this.handleChange(e.target.value)}></input>
-        <p>{this.state.message}</p>
+    return(
+      <div className = "App">
+      {listOfFriends}
       </div>
-    );
+    )
   }
-}
+};
 
 export default App;
 
