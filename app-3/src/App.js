cde34dc8713716c26fa212c4339d48cf651d2091
Jonathan Waller <waller.j27@gmail.com>
2018-05-07 23:41:35 -0500

    set 1 q-3 final

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,19 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(){
+    super();
+    this.state={
+      filterText:'',
+      places: ["Illinois",'Florida','Missouri','Texas']
+    }
+  };
+
+  handleChange(value){
+    this.setState({filterText:value})
   }
-}
+    
+  render(){
+    let listOfPlaces = this.state.places.filter((value,index)=>{
+    return value.includes (this.state.filterText);
+  }).map((element, index) => {
+    return <h2 key={index}>{element}</h2>
+  })
+
+    return(
+      <div className = "App">
+      <input onChange={(e)=>this.handleChange(e.target.value) } ></input>
+      {listOfPlaces}
+      </div>
+    )
+  }
+};
 
 export default App;
